d3a678a40e056f0ac0626ee9a5723cb913fc599a
AlekseyB <151134654+AlekseyBulgarin@users.noreply.github.com>
2025-11-30 16:42:56 +0500

    Update ConsoleApplication11.cpp

--- ConsoleApplication11.cpp
+++ ConsoleApplication11.cpp
@@ -1,6 +1,23 @@
-﻿#include <iostream>
+#include <iostream>
+
+using namespace std;
+
+template<typename T>
+T average(T array[], int size) {
+	T sum = T();
+	for (int i = 0; i < size; i++) {
+		sum += array[i];
+	}
+	return sum / T(size);
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int intArr[] = { 1, 2, 3, 4, 5 };
+	double avgInt = average(intArr, 5);
+	std::cout << "Average int: " << avgInt << std::endl;
+
+	double dblArr[] = { 1.5, 2.5, 3.5 };
+	double avgDbl = average(dblArr, 3);
+	std::cout << "Average double: " << avgDbl << std::endl;
 }
