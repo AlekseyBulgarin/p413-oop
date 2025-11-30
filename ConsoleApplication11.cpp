9ad767b67790ae46d1694a11faddb5055f4f755e
AlekseyB <151134654+AlekseyBulgarin@users.noreply.github.com>
2025-11-30 17:52:35 +0500

    Update ConsoleApplication11.cpp
    first commit

--- ConsoleApplication11.cpp
+++ ConsoleApplication11.cpp
@@ -1,23 +1,46 @@
 #include <iostream>
-
-using namespace std;
+#include <cmath>
 
 template<typename T>
-T average(T array[], int size) {
-	T sum = T();
-	for (int i = 0; i < size; i++) {
-		sum += array[i];
+T equationLine(T a, T b) {
+	T x = T();
+	x = -b / a;
+	return x;
+}
+
+template<typename T2>
+T2 equationQuad(T2 a, T2 b, T2 c) {
+	T2 x1 = T2();
+	T2 x2 = T2();
+	T2 D = T2();
+
+	if (a == 0) {
+		std::cout << "a cannot = 0." << std::endl;
+		return 1;
 	}
-	return sum / T(size);
+
+	D = b * b - 4 * a * c;
+
+	if (D > 0) {
+		x1 = (-b - sqrt(D)) / 2;
+		x2 = (-b + sqrt(D)) / 2;
+		std::cout << "x1 = " << x1 << " and x2 = " << x2;
+	}
+	else if (D == 0) {
+		x1 = -b / 2;
+		x2 = x1;
+		std::cout << "x1 and x2 = " << x1;
+	}
+	else {
+		std::cout << "No equation roots.";
+	}
 }
 
 int main()
 {
-	int intArr[] = { 1, 2, 3, 4, 5 };
-	double avgInt = average(intArr, 5);
-	std::cout << "Average int: " << avgInt << std::endl;
+	std::cout << "Linear equation result: " << equationLine(2, 4) << std::endl;
 
-	double dblArr[] = { 1.5, 2.5, 3.5 };
-	double avgDbl = average(dblArr, 3);
-	std::cout << "Average double: " << avgDbl << std::endl;
+	std::cout << "\n";
+
+	equationQuad(1, 3, 10);
 }
