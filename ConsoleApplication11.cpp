bbe074eacf3c63362396a93d1d9f1ff9f49dca33
AlekseyB <151134654+AlekseyBulgarin@users.noreply.github.com>
2025-11-30 17:11:11 +0500

    Update ConsoleApplication11.cpp
    first commit

--- ConsoleApplication11.cpp
+++ ConsoleApplication11.cpp
@@ -3,21 +3,13 @@
 using namespace std;
 
 template<typename T>
-T average(T array[], int size) {
-	T sum = T();
-	for (int i = 0; i < size; i++) {
-		sum += array[i];
-	}
-	return sum / T(size);
+T minValue(T a, T b) {
+	return (a < b) ? a : b;
 }
 
 int main()
 {
-	int intArr[] = { 1, 2, 3, 4, 5 };
-	double avgInt = average(intArr, 5);
-	std::cout << "Average int: " << avgInt << std::endl;
+	setlocale(LC_ALL, "RU");
 
-	double dblArr[] = { 1.5, 2.5, 3.5 };
-	double avgDbl = average(dblArr, 3);
-	std::cout << "Average double: " << avgDbl << std::endl;
+	cout << minValue(1, 2);
 }
